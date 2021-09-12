37a6977291aebdeda67c00154bf2bd22c2024999
Nikhil Sachan <nsachan1983@gmail.com>
2021-09-12 17:49:42 +0530

    added footer

--- blog_frontend/src/App.js
+++ blog_frontend/src/App.js
@@ -8,6 +8,7 @@ import FitnessCalculator from "./pages/FitnessCalculator";
 import Contact from "./pages/Contact";
 import Notes from "./pages/Notes";
 import Error from "./pages/Error";
+import Footer from "./pages/Footer";
 import Chest from "./pages/exercises/Chest";
 import Back from "./pages/exercises/Back";
 import Arms from "./pages/exercises/Arms";
@@ -24,58 +25,59 @@ function App() {
   return (
     <Router>
       <NavBar />
-        <Switch>
-          <Route path="/" exact={true}>
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/exercises">
-            <Exercises />
-          </Route>
-          <Route path="/chest">
-            <Chest />
-          </Route>
-          <Route path="/back">
-            <Back />
-          </Route>
-          <Route path="/arms">
-            <Arms />
-          </Route>
-          <Route path="/shoulders">
-            <Shoulders />
-          </Route>
-          <Route path="/legs">
-            <Legs />
-          </Route>
-          <Route path="/cardio">
-            <Cardio />
-          </Route>
-          <Route path="/abs">
-            <Abs />
-          </Route>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/exercises">
+          <Exercises />
+        </Route>
+        <Route path="/chest">
+          <Chest />
+        </Route>
+        <Route path="/back">
+          <Back />
+        </Route>
+        <Route path="/arms">
+          <Arms />
+        </Route>
+        <Route path="/shoulders">
+          <Shoulders />
+        </Route>
+        <Route path="/legs">
+          <Legs />
+        </Route>
+        <Route path="/cardio">
+          <Cardio />
+        </Route>
+        <Route path="/abs">
+          <Abs />
+        </Route>
 
-          <Route path="/articles">
-            <Articles />
-          </Route>
-          <Route path="/fitnessmania">
-            {/* //intro page */}
-            <FitnessMania />
-          </Route>
-          <Route path="/fitnesscalculator">
-            <FitnessCalculator />
-          </Route>
-          <Route path="/notes">
-            <Notes />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="*">
-            <Error />
-          </Route>
-        </Switch>
+        <Route path="/articles">
+          <Articles />
+        </Route>
+        <Route path="/fitnessmania">
+          {/* //intro page */}
+          <FitnessMania />
+        </Route>
+        <Route path="/fitnesscalculator">
+          <FitnessCalculator />
+        </Route>
+        <Route path="/notes">
+          <Notes />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer/>
     </Router>
   );
 }
---
+++ blog_frontend/src/pages/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer class="container">
+      <p class="float-right">
+        <a href="#">Back to top</a>
+      </p>
+      <p>
+        © 2020-2021 Company, Inc. · <a href="#">Privacy</a> ·{" "}
+        <a href="#">Terms</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
