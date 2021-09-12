cf8bfc5f68101102b1a6bda8b70d104684d0347d
Nikhil Sachan <nsachan1983@gmail.com>
2021-09-12 10:59:04 +0530

    completed form frontend

--- blog_frontend/src/pages/Contact.js
+++ blog_frontend/src/pages/Contact.js
@@ -23,6 +23,7 @@ const Contact = () => {
     setData((data) => {
       return [...data, formData];
     });
+    console.log(data);
     setName("");
     setEmail("");
     setProfession("");
@@ -30,8 +31,40 @@ const Contact = () => {
     setFeedback("");
   };
 
+  const nameEvent = (event) => {
+    setName(event.target.value);
+  };
+
+  const emailEvent = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const profEvent = (event) => {
+    setProfession(event.target.value);
+  };
+
+  const doubtEvent = (event) => {
+    setDoubt(event.target.value);
+  };
+
+  const feedEvent = (event) => {
+    setFeedback(event.target.value);
+  };
+
   return (
     <div className="container">
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">
+          Name
+        </label>
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="1"
+          value={name}
+          onChange={nameEvent}
+        ></textarea>
+      </div>
       <div className="mb-3">
         <label htmlFor="exampleFormControlInput1" className="form-label">
           Email address
@@ -41,18 +74,53 @@ const Contact = () => {
           className="form-control"
           id="exampleFormControlInput1"
           placeholder="name@example.com"
+          value={email}
+          onChange={emailEvent}
         />
+        <small id="emailHelp" className="form-text text-muted">
+          We'll never share your email with anyone else.
+        </small>
       </div>
       <div className="mb-3">
         <label htmlFor="exampleFormControlTextarea1" className="form-label">
-          Example textarea
+          Your Profession
+        </label>
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="1"
+          value={profession}
+          onChange={profEvent}
+          placeholder="e.g. Student, Office Person, etc..,"
+        ></textarea>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">
+          Elaborate your doubt
         </label>
         <textarea
           className="form-control"
           id="exampleFormControlTextarea1"
           rows="3"
+          value={doubt}
+          onChange={doubtEvent}
         ></textarea>
       </div>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">
+          Tell us how to improve
+        </label>
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="5"
+          value={feedback}
+          onChange={feedEvent}
+        ></textarea>
+      </div>
+      <button onClick={handleSubmit} type="submit" className="btn btn-primary">
+        Submit
+      </button>
     </div>
   );
 };
--- blog_frontend/src/pages/FitnessMania.js
+++ blog_frontend/src/pages/FitnessMania.js
@@ -4,7 +4,7 @@ import "./css/FitnessMania.css";
 const FitnessMania = () => {
   return (
     <div className="container">
-      <h1 id="heading">Welcome to FitnessMania</h1>
+      <h1>Welcome to FitnessMania</h1>
     </div>
   );
 };
